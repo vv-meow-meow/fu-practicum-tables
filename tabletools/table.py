411fbc5a68e7999338af4e1913b691f65450fb43
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-11 15:30:33 +0300

    Add get_rows_by_number(), get_rows_by_index(), get_column_types()

--- tabletools/table.py
+++ tabletools/table.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 class Table:
     def __init__(self, data=None):
         self._data = data or []
@@ -24,3 +27,50 @@ class Table:
         for row in self._data:
             row_str = " | ".join(f"{str(item):<{widths[i]}}" for i, item in enumerate(row))
             print(row_str)
+
+    def get_rows_by_number(self, start, stop=None, copy_table=False):
+        match stop:
+            case None:
+                result = [self._data[start]]
+            case -1:
+                result = self._data[start:]
+            case _:
+                result = self._data[start - 1:stop]
+
+        return deepcopy(result) if copy_table else result
+
+    def get_rows_by_index(self,
+                          *values,
+                          copy_table=False):
+        result = [row for row in self._data if row[0] in values]
+        if copy_table: result = deepcopy(result)
+        return result
+
+    def _infer_type(self, column):
+        types = set()
+        for value in column:
+            if value.isdigit():
+                types.add(int)
+            else:
+                try:
+                    float(value)
+                    types.add(float)
+                except ValueError:
+                    if value.lower() in ('true', 'false'):
+                        types.add(bool)
+                    else:
+                        types.add(str)
+
+        if len(types) == 1:
+            return next(iter(types))
+        return str
+
+    def get_column_types(self, by_number=True):
+        if not self._data: return {}
+
+        column_types = {}
+        for index, column in enumerate(zip(*self._data[1:])):
+            inferred_type = self._infer_type(column)
+            column_name = index if by_number else self._data[0][index]
+            column_types[column_name] = inferred_type
+        return column_types
