57999dd3c8e600ab4cd0ab820f77b62142bb3eb1
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-14 20:31:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from tabletools import Table, CSVHandler, PickleHandler
+from tabletools import Table, CSVHandler, TXTHandler, PickleHandler
 
 if __name__ == '__main__':
     table = Table()
-    table.load_data("names.csv", CSVHandler)
+    table.load_data("./table_samples/customers-100.csv", CSVHandler)
 
     table.print_table()
 
     table.save_data("output.pkl", PickleHandler)
+    table.save_data("output.txt", TXTHandler)
